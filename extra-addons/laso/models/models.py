6aa8484173e0c5bcd3eb5c0db4e877c44af14c94
Juan Z <juancarlosz1150@gmail.com>
2021-12-10 16:20:17 -0400

    dia 4 parte 6 commit

--- extra-addons/laso/models/models.py
+++ extra-addons/laso/models/models.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+import datetime
 
 class Visit(models.Model):
     #atributos obligatorios del modelo
@@ -22,6 +23,33 @@ class Visit(models.Model):
     #En python el objeto que referencia una instancia se identifica como self
      def toggle_state(self):
        self.done = not self.done
+
+        #ORM
+     def f_create(self):
+        visit = {
+            'name': 'ORM test',
+            'customer': 1,
+            'date': str(datetime.date(2021, 12, 10)),
+            'type': 'P',
+            'done': False
+        } 
+        print(visit)
+        self.env['laso.visit'].create(visit)
+
+     def f_search_update(self):
+        visit = self.env['laso.visit'].search([('name', '=', 'ORM test')])
+        print('search()', visit, visit.name)
+
+        visit_b = self.env['laso.visit'].browse(visit[:1])
+        print('browse()', visit_b, visit_b.name)
+
+        visit.write({
+            'name': 'ORM test write uhu'
+        })
+
+     def f_delete(self):
+        visit = self.env['laso.visit'].browse([8])
+        visit.unlink()
 
   #   value = fields.Integer()
   #   value2 = fields.Float(compute="_value_pc", store=True)
