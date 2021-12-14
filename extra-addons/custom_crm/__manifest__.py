7882ea4151f710ada4d65303e4797aee02ac41e3
Juan Z <juancarlosz1150@gmail.com>
2021-12-14 09:31:29 -0400

    dia 7 empieza la adv

---
+++ extra-addons/custom_crm/__manifest__.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "custom_crm",
+
+    'summary': """
+        Módulo CRM para la gestión de visitas""",
+
+    'description': """
+        Módulo CRM para la gestión de visitas...
+    """,
+
+    'author': "Curso Odoo",
+    'website': "http://www.odoo.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base', 'sale_management'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/views.xml',
+        'views/templates.xml',
+        'reports/visit.xml'
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
